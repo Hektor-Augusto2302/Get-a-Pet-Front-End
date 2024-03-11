2b3d0a7188dd9d58465ec11e8427fcc10d3a2b15
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-03-11 09:36:43 -0300

    feat(navbar finished)

--- src/components/layouts/Navbar/Navbar.jsx
+++ src/components/layouts/Navbar/Navbar.jsx
@@ -1,62 +1,52 @@
-import React, { useState } from 'react';
 import Logo from '../../../assets/img/logo.png';
 import { Context } from '../../../context/UserContext';
 import { useContext } from 'react';
-import './Navbar.css';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     const { authenticated, logout } = useContext(Context);
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
 
     return (
         <nav className='navbar d-flex justify-content-space-between'>
-            <div className='navbarLogo d-flex justify-content-center align-items-center'>
-                <img className='dog' src={Logo} alt="Get a Pet" />
-                <h2>Get a Pet</h2>
+            <div className="container">
+                <div className='navbarLogo d-flex justify-content-center align-items-center'>
+                    <img className='dog' src={Logo} alt="Get a Pet" />
+                    <h2>Get a Pet</h2>
+                </div>
+                <ul className='d-flex justify-content-space-around p-0 m-0 align-items-center navbarUl'>
+                    <li className='navbarLi'>
+                        <Link className='navbarLink' to='/'>Adotar</Link>
+                    </li>
+                    {authenticated ?
+                        (
+                            <>
+                                <li className='navbarLi'>
+                                    <Link className='navbarLink' to='/pet/mypets'>Meus Pets</Link>
+                                </li>
+                                <li className='navbarLi'>
+                                    <Link className='navbarLink' to='/pet/adoptions'>Minhas Adoções</Link>
+                                </li>
+                                <li className='navbarLi'>
+                                    <Link className='navbarLink' to='/user/profile'>Perfil</Link>
+                                </li>
+                                <li onClick={logout} className='navbarLi'>
+                                    Sair
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className='navbarLi'>
+                                    <Link className='navbarLink' to='/login'>Entrar</Link>
+                                </li>
+                                <li className='navbarLi'>
+                                    <Link className='navbarLink' to='/register'>Registrar</Link>
+                                </li>
+                            </>
+                        )
+                    }
+                </ul>
             </div>
-
-            <input type="checkbox" id="nav-toggle" className="nav-toggle visually-hidden" checked={menuOpen} onChange={toggleMenu} />
-            <label htmlFor="nav-toggle" className="nav-toggle-label">
-                <span></span>
-            </label>
-
-            <ul className={`d-flex justify-content-space-around p-0 m-0 align-items-center navbarUl ${menuOpen ? 'open' : ''}`}>
-                <li className='navbarLi'>
-                    <Link className='navbarLink' to='/'>Adotar</Link>
-                </li>
-                {authenticated ?
-                    (
-                        <>
-                            <li className='navbarLi'>
-                                <Link className='navbarLink' to='/pet/mypets'>Meus Pets</Link>
-                            </li>
-                            <li className='navbarLi'>
-                                <Link className='navbarLink' to='/pet/adoptions'>Minhas Adoções</Link>
-                            </li>
-                            <li className='navbarLi'>
-                                <Link className='navbarLink' to='/user/profile'>Perfil</Link>
-                            </li>
-                            <li onClick={logout} className='navbarLi'>
-                                Sair
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className='navbarLi'>
-                                <Link className='navbarLink' to='/login'>Entrar</Link>
-                            </li>
-                            <li className='navbarLi'>
-                                <Link className='navbarLink' to='/register'>Registrar</Link>
-                            </li>
-                        </>
-                    )
-                }
-            </ul>
         </nav>
     );
 };
